62e847d44f7a79adf8d19bf40481a6521c752596
Adam Chlebek <CHLEBEAD@mail.gvsu.edu>
2018-07-18 14:59:51 -0400

    Add project files.

---
+++ TicTacToe/Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnTurn = New System.Windows.Forms.Button()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblTurn = New System.Windows.Forms.Label()
+        Me.btnNewGame = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.btn8, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn7, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn6, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn5, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn4, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn3, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn2, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn1, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.btn0, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(67, 82)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(456, 403)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'btn8
+        '
+        Me.btn8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn8.Location = New System.Drawing.Point(303, 267)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(150, 133)
+        Me.btn8.TabIndex = 8
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn7.Location = New System.Drawing.Point(153, 267)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(144, 133)
+        Me.btn7.TabIndex = 7
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn6.Location = New System.Drawing.Point(3, 267)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(144, 133)
+        Me.btn6.TabIndex = 6
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn5.Location = New System.Drawing.Point(303, 135)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(150, 126)
+        Me.btn5.TabIndex = 5
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn4.Location = New System.Drawing.Point(153, 135)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(144, 126)
+        Me.btn4.TabIndex = 4
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn3.Location = New System.Drawing.Point(3, 135)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(144, 126)
+        Me.btn3.TabIndex = 3
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn2.Location = New System.Drawing.Point(303, 3)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(150, 126)
+        Me.btn2.TabIndex = 2
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn1.Location = New System.Drawing.Point(153, 3)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(144, 126)
+        Me.btn1.TabIndex = 1
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btn0.Location = New System.Drawing.Point(3, 3)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(144, 126)
+        Me.btn0.TabIndex = 0
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnTurn
+        '
+        Me.btnTurn.Enabled = False
+        Me.btnTurn.Location = New System.Drawing.Point(236, 527)
+        Me.btnTurn.Name = "btnTurn"
+        Me.btnTurn.Size = New System.Drawing.Size(86, 40)
+        Me.btnTurn.TabIndex = 1
+        Me.btnTurn.UseVisualStyleBackColor = True
+        '
+        'lblHeader
+        '
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.Font = New System.Drawing.Font("Microsoft Himalaya", 50.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.Location = New System.Drawing.Point(58, 9)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(494, 67)
+        Me.lblHeader.TabIndex = 2
+        Me.lblHeader.Text = "Welcome to Tic Tac Toe"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(144, Byte), Integer), CType(CType(183, Byte), Integer), CType(CType(52, Byte), Integer))
+        Me.Panel1.Location = New System.Drawing.Point(13, 82)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(48, 400)
+        Me.Panel1.TabIndex = 3
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Maroon
+        Me.Panel2.Location = New System.Drawing.Point(529, 82)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(48, 400)
+        Me.Panel2.TabIndex = 4
+        '
+        'lblTurn
+        '
+        Me.lblTurn.AutoSize = True
+        Me.lblTurn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTurn.Location = New System.Drawing.Point(232, 582)
+        Me.lblTurn.Name = "lblTurn"
+        Me.lblTurn.Size = New System.Drawing.Size(95, 15)
+        Me.lblTurn.TabIndex = 5
+        Me.lblTurn.Text = "#####'s Turn!"
+        '
+        'btnNewGame
+        '
+        Me.btnNewGame.Location = New System.Drawing.Point(502, 581)
+        Me.btnNewGame.Name = "btnNewGame"
+        Me.btnNewGame.Size = New System.Drawing.Size(75, 23)
+        Me.btnNewGame.TabIndex = 6
+        Me.btnNewGame.Text = "New Game"
+        Me.btnNewGame.UseVisualStyleBackColor = True
+        Me.btnNewGame.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(589, 616)
+        Me.Controls.Add(Me.btnNewGame)
+        Me.Controls.Add(Me.lblTurn)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.btnTurn)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnTurn As Button
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents lblTurn As Label
+    Friend WithEvents btnNewGame As Button
+End Class
---
+++ TicTacToe/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+    Dim green As Integer = 1
+    Dim red As Integer = 2
+    Dim curTurn As Integer
+    Dim colGreen As New Color()
+    Dim winner As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        colGreen = Color.FromArgb(144, 183, 52)
+        Dim r As Random = New Random
+
+        curTurn = r.Next(1, 3)
+
+        If curTurn = green Then
+            btnTurn.BackColor = colGreen
+        Else
+            btnTurn.BackColor = Color.Maroon
+        End If
+    End Sub
+
+    Private Sub MultipleButtons_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click
+        Dim btn As Button = CType(sender, Button)
+        Dim win As Boolean
+
+        If curTurn = green Then
+            btn.BackColor = colGreen
+            curTurn = red
+            btnTurn.BackColor = Color.Maroon
+            win = CheckWinner()
+        Else
+            btn.BackColor = Color.Maroon
+            curTurn = green
+            btnTurn.BackColor = colGreen
+            win = CheckWinner()
+        End If
+
+        If win Then
+            If winner = 1 Then
+                MsgBox("Winner is Green!")
+                btnNewGame.Visible = True
+            Else
+                MsgBox("Winner is Red!")
+                btnNewGame.Visible = True
+            End If
+        End If
+
+    End Sub
+
+    Public Function CheckWinner()
+        If btn0.BackColor = Color.Maroon And btn1.BackColor = Color.Maroon And btn2.BackColor = Color.Maroon Then
+            winner = red
+            Return True
+        ElseIf btn0.BackColor = Color.Maroon And btn3.BackColor = Color.Maroon And btn6.BackColor = Color.Maroon Then
+            winner = red
+            Return True
+        ElseIf btn0.BackColor = Color.Maroon And btn4.BackColor = Color.Maroon And btn8.BackColor = Color.Maroon Then
+            winner = red
+            Return True
+        ElseIf btn1.BackColor = Color.Maroon And btn4.BackColor = Color.Maroon And btn7.BackColor = Color.Maroon Then
+            winner = red
+            Return True
+        ElseIf btn2.BackColor = Color.Maroon And btn4.BackColor = Color.Maroon And btn6.BackColor = Color.Maroon Then
+            winner = red
+            Return True
+        ElseIf btn2.BackColor = Color.Maroon And btn5.BackColor = Color.Maroon And btn8.BackColor = Color.Maroon Then
+            winner = red
+            Return True
+        ElseIf btn6.BackColor = Color.Maroon And btn7.BackColor = Color.Maroon And btn8.BackColor = Color.Maroon Then
+            winner = red
+            Return True
+        ElseIf btn0.BackColor = colGreen And btn1.BackColor = colGreen And btn2.BackColor = colGreen Then
+            winner = green
+            Return True
+        ElseIf btn0.BackColor = colGreen And btn3.BackColor = colGreen And btn6.BackColor = colGreen Then
+            winner = green
+            Return True
+        ElseIf btn0.BackColor = colGreen And btn4.BackColor = colGreen And btn8.BackColor = colGreen Then
+            winner = green
+            Return True
+        ElseIf btn1.BackColor = colGreen And btn4.BackColor = colGreen And btn7.BackColor = colGreen Then
+            winner = green
+            Return True
+        ElseIf btn2.BackColor = colGreen And btn4.BackColor = colGreen And btn6.BackColor = colGreen Then
+            winner = green
+            Return True
+        ElseIf btn2.BackColor = colGreen And btn5.BackColor = colGreen And btn8.BackColor = colGreen Then
+            winner = green
+            Return True
+        ElseIf btn6.BackColor = colGreen And btn7.BackColor = colGreen And btn8.BackColor = colGreen Then
+            winner = green
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Private Sub btnNewGame_Click(sender As Object, e As EventArgs) Handles btnNewGame.Click
+        Application.Restart()
+    End Sub
+End Class
